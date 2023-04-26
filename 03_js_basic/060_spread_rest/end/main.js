c0df0560f7ad387555fb6d087dd0cf57422e34a5
daichi0918 <kayasugadaichi@icloud.com>
2023-04-26 22:10:42 +0900

    commit

--- 03_js_basic/060_spread_rest/end/main.js
+++ 03_js_basic/060_spread_rest/end/main.js
@@ -4,15 +4,14 @@ const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 const result = Math.max(...nums);
 console.log(result);
 
-
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1, 10, ...arr2 ];
+let newArr = [...arr1, 10, ...arr2];
 let newArr1 = arr1;
 console.log(newArr);
 
 const obj = {
-  name: "Tom",
+  name: 'Tom',
   age: 22,
 };
 const newObj = { ...obj };
@@ -21,7 +20,7 @@ newObj.name = 'John';
 console.log(newObj, obj);
 
 const restA = (...argA) => console.log(argA);
-restA(1, 3)
+restA(1, 3);
 
 const restB = (n, ...argB) => console.log(argB);
-restB(1, 3, 4)
+restB(1, 3, 4);
